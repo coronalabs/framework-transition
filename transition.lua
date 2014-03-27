9b9b26b37d2618165039bb5313d735134b2e035e
Alexandru Frangeti <alex@coronalabs.com>
2014-03-27 20:09:25 +0100

    Transition 2.0: Maintenance.

--- transition.lua
+++ transition.lua
@@ -609,7 +609,7 @@ function lib:enterFrame ( event )
 	end
 	lib._transitionTable = currentActiveTweens
 	-- also restore the _enterFrameTweens variable here
-	lib._enterFrameTweens = lib._transitionTable
+	lib._enterFrameTweens = currentActiveTweens
 
 	-- TODO: Should also unregister when there are only paused transitions
 	if #currentActiveTweens == 0 then
